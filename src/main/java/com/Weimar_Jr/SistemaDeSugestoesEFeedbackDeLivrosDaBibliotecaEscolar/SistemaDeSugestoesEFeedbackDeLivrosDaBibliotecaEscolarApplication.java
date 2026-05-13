1918d54a1d57f366b862b29fd92f28df737c3363
WeimarJr <weimarvlademirojunior@gmail.com>
2026-05-13 12:30:46 -0400

    feito todos os packeges necessarios

---
+++ src/main/java/com/Weimar_Jr/SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolar/SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.Weimar_Jr.SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Weimar_Jr/SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolar/SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Weimar_Jr.SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
