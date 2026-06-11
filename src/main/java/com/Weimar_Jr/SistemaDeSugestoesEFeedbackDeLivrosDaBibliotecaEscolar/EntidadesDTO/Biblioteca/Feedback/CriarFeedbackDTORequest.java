815c11472a33a76a6b003106fbf0ce18cd298132
WeimarJr <weimarvlademirojunior@gmail.com>
2026-06-10 21:13:49 -0400

    agora an criação de feedback valida se é no minimo 0 e maximo 5

--- src/main/java/com/Weimar_Jr/SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolar/EntidadesDTO/Biblioteca/Feedback/CriarFeedbackDTORequest.java
+++ src/main/java/com/Weimar_Jr/SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolar/EntidadesDTO/Biblioteca/Feedback/CriarFeedbackDTORequest.java
@@ -1,8 +1,8 @@
 package com.Weimar_Jr.SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolar.EntidadesDTO.Biblioteca.Feedback;
 
-import com.Weimar_Jr.SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolar.EntidadesDTO.Biblioteca.Livro.LivroDTOResponse;
-import com.Weimar_Jr.SistemaDeSugestoesEFeedbackDeLivrosDaBibliotecaEscolar.EntidadesDTO.UsuariosDTO.Aluno.AlunoDTOResponse;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -10,6 +10,8 @@ public record CriarFeedbackDTORequest(
         @NotBlank @JsonProperty("comentario")
         String comentario,
         @NotNull @JsonProperty("avaliacao")
+        @DecimalMax(value = "5.0", message = "Avaliação deve ser no maximo 5")
+        @DecimalMin(value = "0.0", message = "Avaliaçãod eve ser no minimo 0")
         Double avaliacao,
         @NotNull @JsonProperty("nome_do_aluno_visivel")
         Boolean nomeDoAlunoVisivel,
